05cf9ef719ba0a4a83540a5340584bfd98953fbe
nikunjthesiya <nikunjthesiya.js@gmail.com>
2023-04-27 21:30:42 +0530

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { getToken, removeToken } from "@/constants/token";
+import { AiFillHome } from "react-icons/ai";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -31,14 +32,21 @@ const Navbar = ({ token }) => {
 						alt="McMine University Logo"
 					/>
 				</Link>
-				{logoutBtn && router?.asPath !== "/login" && (
-					<button
-						className={`bg-[#01608A] text-white px-6 flex items-center justify-center text-center text-sm py-4 rounded-xl`}
-						onClick={() => handleLogout()}
-					>
-						Logout
-					</button>
-				)}
+				<div className="flex items-center gap-2">
+					<Link href="https://www.nord.no/en/Student/">
+						<div className="p-4 bg-white block ml-6 text-[#01608A] md:ml-14 rounded-lg">
+							<AiFillHome />
+						</div>
+					</Link>
+					{logoutBtn && router?.asPath !== "/login" && (
+						<button
+							className={`bg-[#01608A] text-white px-6 flex items-center justify-center text-center text-sm py-4 rounded-xl`}
+							onClick={() => handleLogout()}
+						>
+							Logout
+						</button>
+					)}
+				</div>
 			</div>
 		</div>
 	);
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,16 +3,12 @@ import App from "next/app";
 import { getToken } from "@/constants/token";
 import "@/styles/globals.css";
 import Footer from "@/components/Footer";
-import { IoArrowBackOutline } from "react-icons/io5";
 
 export default function MyApp({ Component, pageProps, token }) {
 	return (
 		<div className="bg-[url('/nord-build.png')] bg-no-repeat bg-cover w-100 min-h-screen flex flex-col gap-8 ">
 			<Navbar token={token} />
 			<div className="max-w-[1440px] flex flex-col items-start">
-				<div className="p-4 bg-white block ml-6 mb-6 text-[#01608A] md:ml-14 rounded-full">
-					<IoArrowBackOutline />
-				</div>
 				<Component {...pageProps} />
 			</div>
 			<Footer />
